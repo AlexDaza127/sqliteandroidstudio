67bca98c5d1e771d681cc83d6842163a37352610
AlexDaza27 <michaeldaza90@gmail.com>
2020-11-12 18:37:26 -0500

    Se crea los métodos filtros para la lista

--- app/src/main/java/com/example/cursoandroidsqlite/PrincipalActivity.java
+++ app/src/main/java/com/example/cursoandroidsqlite/PrincipalActivity.java
@@ -4,6 +4,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,8 @@ import openhelper.SQLiteBaseDatos;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    Button btnConsultarUser, btnMostrarTodos;
+    EditText edtNomCon;
     ArrayList<UsuariosVo> listDatos;
     RecyclerView rcvUsuarios;
     SQLiteBaseDatos helper = new SQLiteBaseDatos(this, "BD1", null, 1);
@@ -28,12 +32,27 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         rcvUsuarios = (RecyclerView)findViewById(R.id.rcvUsuarios);
+        btnConsultarUser = (Button)findViewById(R.id.btnConsultarUser);
+        btnMostrarTodos = (Button)findViewById(R.id.btnMostrarTodos);
+        edtNomCon=(EditText)findViewById(R.id.edtNomCon);
         rcvUsuarios.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
         listDatos = new ArrayList<UsuariosVo>();
 
-        consultarUsuarios();
-
         AdapterDatos adaptador = new AdapterDatos(listDatos);
+
+        btnConsultarUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consultarPorUser(edtNomCon.getText().toString());
+            }
+        });
+
+        btnMostrarTodos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consultarUsuarios();
+            }
+        });
 
         adaptador.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -49,6 +68,7 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     public void consultarUsuarios(){
+        helper.abrir();
         SQLiteDatabase db = helper.getReadableDatabase();
         UsuariosVo usuario = null;
         Cursor cursor = db.rawQuery("SELECT * FROM usuarios", null);
@@ -64,5 +84,27 @@ public class PrincipalActivity extends AppCompatActivity {
             listDatos.add(usuario);
 
         }
+        helper.cerrar();
     }
+
+    public void consultarPorUser(String id){
+        helper.abrir();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        UsuariosVo usuario = null;
+        Cursor cursor = db.rawQuery("SELECT * FROM usuarios WHERE ID = "+id+"", null);
+
+        while(cursor.moveToNext()){
+            usuario = new UsuariosVo();
+            usuario.setId(cursor.getInt(0));
+            usuario.setNombre(cursor.getString(1));
+            usuario.setDistrito(cursor.getString(2));
+            usuario.setCorreo(cursor.getString(3));
+            usuario.setPassword(cursor.getString(4));
+
+            listDatos.add(usuario);
+        }
+        helper.cerrar();
+    }
+
+
 }
